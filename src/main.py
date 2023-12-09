b4f06ed6b965d12666984e85676435c673212c2c
zyr27 <youran1026zeng@163.com>
2023-12-09 16:40:01 +0800

    change movie to music

--- src/main.py
+++ src/main.py
@@ -7,6 +7,7 @@ np.random.seed(555)
 
 parser = argparse.ArgumentParser()
 
+'''
 # movie
 parser.add_argument('--dataset', type=str, default='movie', help='which dataset to use')
 parser.add_argument('--n_epochs', type=int, default=20, help='the number of epochs')
@@ -18,6 +19,7 @@ parser.add_argument('--l2_weight', type=float, default=1e-6, help='weight of l2 
 parser.add_argument('--lr_rs', type=float, default=0.02, help='learning rate of RS task')
 parser.add_argument('--lr_kge', type=float, default=0.01, help='learning rate of KGE task')
 parser.add_argument('--kge_interval', type=int, default=3, help='training interval of KGE task')
+'''
 
 '''
 # book
@@ -33,7 +35,7 @@ parser.add_argument('--lr_kge', type=float, default=2e-5, help='learning rate of
 parser.add_argument('--kge_interval', type=int, default=2, help='training interval of KGE task')
 '''
 
-'''
+
 # music
 parser.add_argument('--dataset', type=str, default='music', help='which dataset to use')
 parser.add_argument('--n_epochs', type=int, default=10, help='the number of epochs')
@@ -45,7 +47,7 @@ parser.add_argument('--l2_weight', type=float, default=1e-6, help='weight of l2 
 parser.add_argument('--lr_rs', type=float, default=1e-3, help='learning rate of RS task')
 parser.add_argument('--lr_kge', type=float, default=2e-4, help='learning rate of KGE task')
 parser.add_argument('--kge_interval', type=int, default=2, help='training interval of KGE task')
-'''
+
 
 parser.add_argument('--workers', type=int, default=3,
                     help='number of data loading workers')
